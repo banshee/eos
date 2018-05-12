31b8aba30e5d1433347a531892be8b2528544565
James Moore <james@restphone.com>
2018-05-12 08:38:32 -0700

    playing

--- contracts/jamestransfer/jamestransfer.cpp
+++ contracts/jamestransfer/jamestransfer.cpp
@@ -1,7 +1,13 @@
+#include <boost/preprocessor/seq/for_each.hpp>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/print.hpp>
 #include <eosiolib/serialize.hpp>
 #include <eosiolib/dispatcher.hpp>
+#include <set>
+#include <iostream>
+#include <tuple>
+#include <map>
+#include <string>
 
 using namespace eosio;
 
@@ -17,6 +23,22 @@ public:
     ///@abi action
     void gimmie(account_name user) {
         print("Hellox ", name{user});
+    }
+
+    int fnord() {
+        auto t = std::make_tuple(1, "x");
+        auto x = std::get<0>(t);
+        return x;
+    }
+
+    int *init();
+
+    int test2() {
+        int *x = init();
+        int *y = init();
+        (y = x, x) = nullptr; // <-- here
+
+        return 0;
     }
 };
 
